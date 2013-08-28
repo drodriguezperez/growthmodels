ee0a43ac5f4e78acd44acccaa020d09fdc87cf90
Daniel Rodríguez Pérez <daniel.rodriguez.perez@gmail.com>
2013-08-29 00:06:05 +0200

    Include Brody growth model

---
+++ R/brody.R
@@ -0,0 +1,49 @@
+##
+##  Brody growth model
+##
+##  Created by Daniel Rodríguez Pérez on 28/8/2013.
+##
+##  Copyright (c) 2013 Daniel Rodríguez Pérez.
+##
+##  This program is free software: you can redistribute it and/or modify
+##  it under the terms of the GNU General Public License as published by
+##  the Free Software Foundation, either version 3 of the License, or
+##  (at your option) any later version.
+##
+##  This program is distributed in the hope that it will be useful,
+##  but WITHOUT ANY WARRANTY; without even the implied warranty of
+##  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##  GNU General Public License for more details.
+##
+##  You should have received a copy of the GNU General Public License
+##  along with this program.  If not, see <http://www.gnu.org/licenses/>
+##
+
+#' Brody growth model
+#'
+#' Computes the Brody growth model
+#' \deqn{ y(t) = \alpha - (\alpha - \beta) exp(- k t) }{ y(t) = \alpha - (\alpha - \beta) * exp(- k * t) }
+#' 
+#' @param t time
+#' @param alpha a curve parameter 
+#' @param beta the size at t = 0
+#' @param k growth rate
+#' 
+#' @usage brody(t, alpha, beta, k)
+#' 
+#' @examples
+#' growth <- brody(0:10, 10, 0.5, 0.3)
+#' 
+#' @references
+#' M. M. Kaps, W. O. W. Herring, and W. R. W. Lamberson, "Genetic and
+#' environmental parameters for traits derived from the Brody growth curve and
+#' their relationships with weaning weight in Angus cattle.," Journal of
+#' Animal Science, vol. 78, no. 6, pp. 1436-1442, May 2000.
+#' 
+#' @rdname brody
+#' @export brody
+#' @aliases brody
+brody <- function(t, alpha, beta, k) {
+  result <- alpha - (alpha - beta) * exp(- k * t)
+  return(result)
+}
--- README.md
+++ README.md
@@ -15,6 +15,7 @@ install_github('growth', 'drodriguezperez')
 
 `growth` includes the following nonlinear growth models
 
+* Brody
 * Chapman-Richards
 * Generalised Logistic
 * Generalised Richard
@@ -34,3 +35,17 @@ install_github('growth', 'drodriguezperez')
 ## License
 
 [GNU General Public License, version 3](http://www.gnu.org/licenses/gpl-3.0.txt)
+
+## References
+
+1. D. Fekedulegn, M. Mac Siurtain, and J. Colbert, "Parameter estimation of
+   nonlinear growth models in forestry," Silva Fennica, vol. 33, no. 4, pp.
+   327-336, 1999.
+2. M. M. Kaps, W. O. W. Herring, and W. R. W. Lamberson, "Genetic and
+   environmental parameters for traits derived from the Brody growth curve and
+   their relationships with weaning weight in Angus cattle.," Journal of
+   Animal Science, vol. 78, no. 6, pp. 1436-1442, May 2000.
+3. A. Khamiz, Z. Ismail, and A. T. Muhammad, "Nonlinear growth models for
+   modeling oil palm yield growth," Journal of Mathematics and Statistics,
+   vol. 1, no. 3, p. 225, 2005.
+4. [Generalised logistic function](http://en.wikipedia.org/wiki/Generalised_logistic_function)
